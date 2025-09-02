7a7623bd744419c48c43069146980000b3382c11
Oliver-Bredmose <oliver.bredmose@gmail.com>
2025-09-02 11:19:48 +0200

    opgave 1 og 2 færdig
    opgave 1 og 2 færdig

--- opgavefiler/js/firstPage.js
+++ opgavefiler/js/firstPage.js
@@ -1,14 +1,44 @@
 /* Opgave 1*/
 
+// din kode her
+
+let firstName = "Aksel"; 
+let aarIvirksomhed = 3;
+ 
+
+ const massege = document.getElementById('message');
+ massege.innerHTML = `<h2>${firstName}</h2> du har været i virksomheden i ${aarIvirksomhed}`
+ console.log (massege);
+
+/* Opgave 2*/
+
 
 // din kode her
 
+const produkter = [
+    { 
+        navn: "Vadnmelon",
+        beskrivelse: "en stor Vandmelon",
+        pris: 100 },
+    { 
+        navn: "æble",
+        beskrivelse: "et grønt æble",
+        pris: 33 },
+    { 
+        navn: "Ananas", 
+        beskrivelse: "en saftig Ananas",
+        pris: 200 }
+]
 
-/* Opgave 1*/
-
-
-// din kode her
-
+ document.getElementById("produktListe").innerHTML = array.forEach(produktListe => {
+    produkter.map
+});(produkt => 
+    <div class="produkt">
+        <h2>${produkt.navn}</h2>
+        <p>${produkt.beskrivelse}</p>
+        <p>Pris: ${produkt.pris} kr.</p>
+    </div>);
+    
 
 
 /* Opgave 3 - skriv videre på koden her: */
